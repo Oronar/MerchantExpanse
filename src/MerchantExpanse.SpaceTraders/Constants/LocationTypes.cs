742ab3daf48cdef9b43928d913550b94ccde51d1
Oronar <kirby91@gmail.com>
2021-03-22 22:38:20 -0400

    feat: Add Gas Giant LocationType

--- src/MerchantExpanse.SpaceTraders/Constants/LocationTypes.cs
+++ src/MerchantExpanse.SpaceTraders/Constants/LocationTypes.cs
@@ -3,6 +3,7 @@
 	public static class LocationTypes
 	{
 		public const string Asteroid = "ASTEROID";
+		public const string GasGiant = "GAS_GIANT";
 		public const string Moon = "MOON";
 		public const string Nebula = "NEBULA";
 		public const string Planet = "PLANET";
